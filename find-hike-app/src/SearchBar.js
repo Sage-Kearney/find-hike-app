455cdeaf1dcaff647ae01f887f935ec328750b95
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-01-24 17:27:39 -0500

    Added hidden file to store API keys Stored API keys in variables

--- find-hike-app/src/SearchBar.js
+++ find-hike-app/src/SearchBar.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default function SearchBar() {
     return (
         <div>
-            this is search bar
+            <form>
+                <input
+                placeholder="this in input"
+                type="text"
+                required
+                >   
+                </input>
+                <button>
+                    this is button
+                </button>
+            </form>
         </div>
     )
 }
