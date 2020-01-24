206477df75b1bc2aeed4a4ffa95085aa07687094
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-01-24 16:20:17 -0500

    Created Header, SearchBar, & SearchResults components & imported them successfully into App.js

--- find-hike-app/src/App.js
+++ find-hike-app/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './SearchBar';
+import Header from './Header';
+import SearchResults from './SearchResults';
 
 function App() {
   return (
     <div>
-      hello
+      <Header />
+      <SearchBar />
+      <SearchResults />
     </div>
   );
 }
