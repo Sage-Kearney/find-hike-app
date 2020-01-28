7c09c750281371e0400744f9fc7f8c46abcb656c
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-01-28 00:49:01 -0500

    Header & search bar are responsive. Added flexbox styling to placeholder results & search results.

--- find-hike-app/src/SearchResults.js
+++ find-hike-app/src/SearchResults.js
@@ -4,26 +4,31 @@ export default function SearchResults(props) {
     console.log(props.results)
 
     return (
-        <div>
+        <div className="results-container">
             {props.results.trails ?   
             props.results.trails.map(hike => {
                 return (
-                    <div key={hike.name}>
+                    <div key={hike.name} className="results-content-container" >
                         <a href={hike.url}>
-                            <img src={hike.imgMedium} alt={hike.name}/>
+                            <div className="image-card" style={{ backgroundImage: `url(${hike.imgMedium})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                            </div>
+                            <div className="content-card">
                             <h3>{hike.name}</h3>
                             <h5>{hike.location}</h5>
                             <p>Length: {hike.length} miles</p>
+                            </div>
                         </a>
                     </div>
                 )
             }) : props.results.map(hike => {
                 return (
-                    <div key={hike.name}>
-                            <img src={hike.imgMedium} alt={hike.name} />
+                    <div key={hike.name} className="results-content-container">
+                        <div className="image-card" style={{ backgroundImage: `url(${hike.imgMedium})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}></div>
+                        <div className="content-card">
                             <h3>{hike.name}</h3>
                             <h5>{hike.location}</h5>
                             <p>Length: {hike.length} miles</p>
+                        </div>
                     </div>
                 )
             })
