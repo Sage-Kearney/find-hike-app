6e07b355ad0d4a0d0097f4cddbb3b0bf5902f047
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-01-28 11:39:58 -0500

    Added links to footer

--- find-hike-app/src/Footer.js
+++ find-hike-app/src/Footer.js
@@ -7,8 +7,8 @@ export default function Footer() {
     return (
         <div className="footer">
             <h6>powered by</h6>
-            <img src="/hiker-api-logo.png" alt="hiker project logo" className="hiker-pjt-logo"></img>
-            <img src="/mapquest-logo.png" alt="mapquest logo" className="mapquest-logo"></img>
+            <a href="https://www.hikingproject.com/"><img src="/hiker-api-logo.png" alt="hiker project logo" className="hiker-pjt-logo"></img></a>
+            <a href="https://www.mapquest.com/"><img src="/mapquest-logo.png" alt="mapquest logo" className="mapquest-logo"></img></a>
         </div>
     )
 }
