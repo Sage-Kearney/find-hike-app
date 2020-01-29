ee7f468dfe23ae050478eec1c3a40213e0ea90bc
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-01-29 01:37:33 -0500

    Added photo attribution in footer. Adjusted styling to ensure footer was still responsive.

--- find-hike-app/src/Footer.js
+++ find-hike-app/src/Footer.js
@@ -6,6 +6,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <div className="footer">
+            <p>Photos by Tim Gouw and Ian Beckley</p>
             <h6>powered by</h6>
             <a href="https://www.hikingproject.com/"><img src="/hiker-api-logo.png" alt="hiker project logo" className="hiker-pjt-logo"></img></a>
             <a href="https://www.mapquest.com/"><img src="/mapquest-logo.png" alt="mapquest logo" className="mapquest-logo"></img></a>
