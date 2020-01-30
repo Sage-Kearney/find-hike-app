3ae272d3b22fcd228c4e95e528afc9dde92a87f7
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-01-30 00:28:04 -0500

    Styling on Show hike done. Need to work on responsiveness.

--- src/Showhike.js
+++ src/Showhike.js
@@ -8,17 +8,25 @@ export default function Showhike(props) {
     console.log(props)
     return (
         <div className="show-hike-container">
-            <p className="back-home-btn"><Link to="/" onClick={props.restPage} >Back to home</Link></p>
+            <p className="back-home-btn"><Link to="/" onClick={props.restPage} >Back to search</Link></p>
             <img src={props.hike.imgMedium} alt={props.hike.name}></img>
             <div className="hike-content">
                 <h2 className="showpageH1">{props.hike.name}</h2>
                 <p>{props.hike.summary}</p>
                 <hr></hr>
-                <h4><span className="attribute">Length:</span> {props.hike.length}</h4>
-                <h4><span className="attribute">Difficulty:</span> {props.hike.difficulty}</h4>
-                <h4><span className="attribute">Community Rating:</span> {props.hike.stars}</h4>
-                <h4><span className="attribute">Location:</span> {props.hike.location}</h4>
-                <h4><span className="attribute">Total ascent:</span> {props.hike.ascent} <span className="attribute">Total descent:</span> {props.hike.descent}</h4>
+                <div className="details-container">
+                    <div>
+                        <h4><span className="attribute">Length:</span> {props.hike.length}</h4>
+                        <h4><span className="attribute">Difficulty:</span> {props.hike.difficulty}</h4>
+                        <h4><span className="attribute">Community Rating:</span> {props.hike.stars}</h4>
+                    </div>
+                    <div>
+                        <h4><span className="attribute">Location:</span> {props.hike.location}</h4>
+                        <h4><span className="attribute">Total ascent:</span> {props.hike.ascent}</h4>
+                        <h4><span className="attribute">Total descent:</span> {props.hike.descent}</h4>
+                    </div>
+                </div>
+                <button><a href={props.hike.url} className="">More Information</a></button>
             </div>
         </div>
     )
